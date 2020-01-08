b12e8b364e26d276e89d40b3d80261b3e74752fa
tkyko13 <>
2020-01-09 00:59:37 +0900

    20200108

--- 202001/08/sketch.js
+++ 202001/08/sketch.js
@@ -1,6 +1,6 @@
 let n1, n2;
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas((w = 800), w);
   n1 = random(100);
   n2 = random(100);
   strokeWeight(5);
@@ -12,3 +12,20 @@ function draw() {
   stroke(random(255), random(255), random(255));
   line(noise(n1) * width, 0, noise(n2) * width, height);
 }
+
+// #つぶやきprocessing
+/*
+let n1,n2;
+setup=_=>{
+createCanvas((w=800),w);
+n1=random(99);
+n2=random(99);
+strokeWeight(5);
+}
+draw=_=>{
+n1+=0.015;
+n2+=0.015;
+stroke(random(255),random(255),random(255));
+line(noise(n1)*width,0,noise(n2)*width,height);
+}
+*/
