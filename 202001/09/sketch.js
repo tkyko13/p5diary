e29c6a06e97dc06423fd991f5f928a42788080a5
tkyko13 <>
2020-01-09 18:41:58 +0900

    20200109

---
+++ 202001/09/sketch.js
@@ -0,0 +1,38 @@
+let n1, n2, colorN;
+let x1 = 0,
+  x2 = 0;
+let sp1 = 1,
+  sp2 = 1;
+const loopNum = 5;
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  n1 = random(100);
+  n2 = random(100);
+  colorN = random();
+  strokeWeight(1);
+  colorMode(HSB, 1, 1, 1, 1);
+}
+
+function draw() {
+  for (let i = 0; i < loopNum; i++) {
+    n1 += 0.015;
+    n2 += 0.015;
+    colorN += 0.001;
+    x1 += sp1;
+    x2 += sp2;
+    if (x1 < 0 || width < x1) {
+      // sp1 = -sp1;
+      // sp2 = -sp2;
+      x1 = 0;
+      x2 = 0;
+      n1 = random(100);
+      n2 = random(100);
+      colorN = random();
+    }
+    // if (x2 < 0 || width < x2) sp2 = -sp2;
+
+    stroke(colorN % 1, 0.6, 0.8, 0.2);
+    // line(x1, noise(n1) * height, x2, noise(n2) * height);
+    line(x1, noise(n1) * height, x2, noise(n2) * height);
+  }
+}
