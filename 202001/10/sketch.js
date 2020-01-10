c34a2915d79d9671810bc288744930c5660b58ec
tkyko13 <>
2020-01-10 19:00:59 +0900

    20200110

--- 202001/10/sketch.js
+++ 202001/10/sketch.js
@@ -14,9 +14,10 @@ function setup() {
 }
 
 function draw() {
+  background(255, 0.01);
   for (let i = 0; i < loopNum; i++) {
-    n1 += 0.015;
-    n2 += 0.015;
+    n1 += 0.1;
+    n2 += 0.1;
     colorN += 0.001;
     x1 += sp1;
     x2 += sp2;
